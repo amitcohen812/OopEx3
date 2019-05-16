0b463ccc9d68d3d91da2e86638fc345315701771
Amitco812 <amitak@post.bgu.ac.il>
2019-05-16 14:23:19 +0300

    new class GameBoard, methods need to be completed by the notes,Player edited

---
+++ src/GameBoard.java
@@ -0,0 +1,40 @@
+
+import java.util.LinkedList;
+
+public class GameBoard{
+    private String pathToBoards;
+    private String isDeterministic;
+    private char [][] gameBoard;
+    private LinkedList<GameUnit> gameUnits;
+
+    public GameBoard(String pathToBoards,String isDeterministic){
+        this.pathToBoards=pathToBoards;
+        this.isDeterministic=isDeterministic;
+        buildBoard();
+        scanBoard();
+    }
+    public GameBoard(String pathToBoards){
+        this.pathToBoards=pathToBoards;
+        buildBoard();
+        scanBoard();
+    }
+
+    private void buildBoard(){ //reads the file from the path and builds the board
+
+    }
+
+    private void distinguishChar(char c){ //checks what kind of slot it is, enemy, player etc..., updates gameunit list
+
+    }
+    private void scanBoard(){ //iterates the whole game board
+        for (int i=0;i<gameBoard.length;i=i+1){
+            for (int j=0;j<gameBoard[i].length;j=j+1){
+                distinguishChar(gameBoard[i][j]);
+            }
+        }
+    }
+    public char[][] getGameBoard() {
+        return gameBoard;
+    }
+
+}
--- src/Player.java
+++ src/Player.java
@@ -5,21 +5,21 @@ public abstract class Player extends GameUnit {
     protected Integer experience;
     protected Integer level;
 
-    public void levelUp(){
-        this.experience=this.experience-50*this.level;
-        this.level=this.level+1;
-        this.health.setHealthPool(this.health.getHealthPool()+10*this.level);
+    public void levelUp() {
+        this.experience = this.experience - 50 * this.level;
+        this.level = this.level + 1;
+        this.health.setHealthPool(this.health.getHealthPool() + 10 * this.level);
         this.health.setCurrentHealth(this.health.getHealthPool());
-        this.attackPoints=this.attackPoints+5*level;
-        this.defensePoints=this.defensePoints+2*level;
+        this.attackPoints = this.attackPoints + 5 * level;
+        this.defensePoints = this.defensePoints + 2 * level;
     }
-    public Player(String name,Health health,Integer attackPoints,Integer defensePoints){
+
+    public Player(String name, Health health, Integer attackPoints, Integer defensePoints) {
         super(name, health, attackPoints, defensePoints);
-        this.position=new Point(0,0);
-        this.level=1;
+        this.position = new Point(0, 0);
+        this.level = 1;
     }
 
-    public void castSpecialAbility(){}
-
-
+    public void castSpecialAbility() {
+    }
 }
