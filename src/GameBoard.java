cd566d25b46978291cf1025f940ac9f298f05a41
Amitco812 <amitak@post.bgu.ac.il>
2019-05-17 18:53:02 +0300

    List of enemies inited

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -14,15 +14,18 @@ public class GameBoard extends Observable{
     public static LinkedList<Enemy> gameUnits;
     public static Point playerPosition;
     public static Player player;
+    private LinkedList<Enemy> possibleEnemies;
 
     public GameBoard(String pathToBoards,String isDeterministic){
         this.pathToBoards=pathToBoards;
         this.isDeterministic=isDeterministic;
+        initEnemies();
         buildBoard();
         scanBoard();
     }
     public GameBoard(String pathToBoards){
         this.pathToBoards=pathToBoards;
+        initEnemies();
         buildBoard();
         scanBoard();
     }
@@ -43,23 +46,43 @@ public class GameBoard extends Observable{
             System.out.println(e.getMessage());
         }
     }
+    private void initEnemies(){
+        possibleEnemies=new LinkedList<>();
+        Monster lannisterSoldier=new Monster("Lannister Soldier",new Health(80,80),8,3,25,'s',3);
+        Monster knight=new Monster("Lannister Knight",new Health(200,200),14,8,50,'k',4);
+        Monster queensGuard=new Monster("Queen's Guard",new Health(400,400),20,15,100,'q',5);
+        Monster wright=new Monster("Wright",new Health(600,600),30,15,100,'z',3);
+        Monster bearWright=new Monster("Bear-Wright",new Health(1000,1000),75,30,250,'b',4);
+        Monster giantWright=new Monster("Giant-Wright",new Health(1500,1500),100,40,500,'g',5);
+        Monster whiteWalker=new Monster("White Walker",new Health(2000,2000),150,50,1000,'w',6);
+        Monster theMountain =new Monster("The Mountain",new Health(1000,1000),60,25,500,'M',6);
+        Monster queenCersei=new Monster("Queen Cersei",new Health(100,100),10,10,1000,'C',1);
+        Monster knightKing=new Monster("Knight King",new Health(5000,5000),300,150,5000,'K',8);
+        Trap bonusTrap=new Trap("Bonus Trap",new Health(1,1),1,1,250,'B',5,6,2);
+        Trap queensTrap=new Trap("Queen's Trap",new Health(250,250),50,10,100,'Q',4,10,4);
+        Trap deathTrap=new Trap("Death Trap",new Health(500,500),100,20,250,'D',6,10,3);
+        possibleEnemies.addFirst(lannisterSoldier);
+        possibleEnemies.addFirst(knight);
+        possibleEnemies.addFirst(queenCersei);
+        possibleEnemies.addFirst(queensGuard);
+        possibleEnemies.addFirst(wright);
+        possibleEnemies.addFirst(bearWright);
+        possibleEnemies.addFirst(giantWright);
+        possibleEnemies.addFirst(whiteWalker);
+        possibleEnemies.addFirst(theMountain);
+        possibleEnemies.addFirst(knightKing);
+        possibleEnemies.addFirst(bonusTrap);
+        possibleEnemies.addFirst(queensTrap);
+        possibleEnemies.addFirst(deathTrap);
 
-    private void distinguishChar(LinkedList<Character> listOfCopm){ //checks what kind of slot it is, enemy, player etc..., updates gameunit list
-        for (Character c: listOfCopm)
-        {
-            if (c!='.'&c!='#'&c!='@'){
-
-            }
-        }
     }
     private void scanBoard(){ //iterates the whole game board
-        LinkedList<Character> listOfComp=new LinkedList<>();
         for (int i=0;i<gameBoard.length;i=i+1){
             for (int j=0;j<gameBoard[i].length;j=j+1){
-                listOfComp.addFirst(gameBoard[i][j]);
+                char c=gameBoard[i][j];
+
             }
         }
-        distinguishChar(listOfComp);
     }
 
     public static void combat(Player attacker, Enemy defender){
@@ -72,7 +95,7 @@ public class GameBoard extends Observable{
     public static void combat(Enemy attacker,Player defender){
         attacker.attack(defender);
         if (defender.health.getCurrentHealth()<=0){
-            // put X on player and end the game.
+            // puts X on player and end the game.
             gameBoard[defender.position.x][defender.position.y]='X';
             //endgame - needs to complete
         }
